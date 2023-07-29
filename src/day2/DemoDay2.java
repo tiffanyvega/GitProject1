e1661288459c36df21303614b3b147a124abd6d1
tiffanyvega <140762970+tiffanyvega@users.noreply.github.com>
2023-07-29 10:50:43 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -15,4 +15,10 @@ public class DemoDay2 {
 		public static void subtract(int a, int b) {
 			System.out.println(a - b);
 		}
+	//add method
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+
+	}
+		
 }
