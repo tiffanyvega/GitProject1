2107aec232f82e6d6f767f09864905159c6618e5
tiffany <tiffany@192.168.0.10>
2023-07-29 10:35:50 -0400

    Implemented add

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,4 +4,15 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
+	
+	//add method
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+		
+	}
+	
+	//subtract
+		public static void subtract(int a, int b) {
+			System.out.println(a - b);
+		}
 }
